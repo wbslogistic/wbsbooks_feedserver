7d6dcb4c999227fab3370d99b0333953f684cdfe
Serghei <agilestep@yahoo.com>
2014-11-08 14:57:17 +0200

    All necessary gems for szko parser collected

--- ozon_feed_extractor.rb
+++ ozon_feed_extractor.rb
@@ -1,29 +1,61 @@
 
 
-require "rubyzip"
-require "parsers/azbuka_parser"
-require 'parsers/exmo_parser'
-require 'parsers/ozon_parser'
-require 'parsers/piter_parser'
-require 'parsers/szko_parser'
 
+
+require 'rubygems'
+require 'require_all'
+
+require 'psych' # for reading yml
+require 'open-uri'
+require 'zip/zip'
+
+ # xls readers
+#require 'creek' # to read large xls
+#require 'roo'
+#require "simple-spreadsheet"
+
+require 'spreadsheet' # best for large xls
+
+
+require_all 'parsers'
+require_all 'models'
+
+
+
+
+
+
+
+p 'start'
+
+@@config  =       Psych.load_file( "./config/config.yml" )
 
 
 def start
+  parser1=  SzkoParser.new
+  parser1.parse
 
+  pasers = [AzbukaParser.new,
+            ExmoParser.new,
+            PiterParser.new,
+            SzkoParser.new,
+             ]
 
-  pasers = [AzbukaParser.new,ExmoParser.new,PiterParser.new,SzkoParser.new,SzkoParser.new]
-
-
-
-
-
-
-
-
-
-
-
-
+ parser.each do |parser|
+   parser.parse
+ end
 end
+
+
+start
+
+
+
+
+
+
+
+
+
+
 
--- parsers/szko_parser.rb
+++ parsers/szko_parser.rb
@@ -1,7 +1,96 @@
+
 class SzkoParser
 
   def parse
+     p 'start_parsing'
+     url=  @@config["szko"]
+     zip_path=  @@config["szko_zip"]
+     szko_path = @@config["szko_path"]
+     szko_xls = @@config["szko_xls"]
+
+
+    #  p "Getting zip from:#{url}"
+    # open('zip_path', 'wb') do |file|
+    #   file << open(url).read
+    # end
+    #
+    #  p ' Unziping #{zip_path}'
+    # unzip_file(zip_path,szko_path)
+
+     p ' Unzip done!'
+
+     get_products_from_xls(szko_xls)
 
   end
 
-end
+
+
+  def  get_products_from_xls(path)
+
+  book = Spreadsheet.open path
+  sheet1 = book.worksheet 0
+    sheet1.each do |row|
+       p row.to_s
+    end
+
+  end
+
+
+
+  # def  get_products_from_xls3(path)
+  #
+  #   s = Roo::Excel.new(path)
+  #
+  #   s.first_row.upto(s.last_row) do |line|
+  #     product= Product.new
+  #     name = s.cell(line, 1)
+  #   end
+  #
+  #
+  #
+  # end
+
+
+  # def get_products_from_xls(path)
+  #
+  #   s = SimpleSpreadsheet::Workbook.read(path)
+  #   s.selected_sheet = s.sheets.first
+  #
+  #   products =[]
+  #   s.first_row.upto(s.last_row) do |line|
+  #     product= Product.new
+  #
+  #     name = s.cell(line, 1, 1)
+  #     second = s.cell(line, 3, 1)
+  #
+  #   end
+  #
+  #
+  #   p ' getting products finished'
+  #
+  # end
+
+  #
+  # def get_products_from_xlsOld2(path)
+  #   creek = Creek::Book.new path
+  #   sheet= creek.sheets[0]
+  #
+  #   sheet.rows.each do |row|
+  #     puts row # => {"A1"=>"Content 1", "B1"=>nil, C1"=>nil, "D1"=>"Content 3"}
+  #   end
+  #
+  #
+  #
+  # end
+
+  # def unzip_file (file, destination)
+  #   Zip::ZipFile.open(file) do |zip_file|
+  #     zip_file.each do |f|
+  #       f_path=File.join(destination, f.name)
+  #       FileUtils.mkdir_p(File.dirname(f_path))
+  #       zip_file.extract(f, f_path) unless File.exist?(f_path)
+  #     end
+  #   end
+  # end
+
+  end
