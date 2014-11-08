789a22c9b1ae94f4972f1be7c837b875441628b8
Serghei <agilestep@yahoo.com>
2014-11-08 14:55:02 +0200

    Adding Product properties

--- models/product.rb
+++ models/product.rb
@@ -1,2 +1,7 @@
 class Product
+  attr_accessor :book_id, :price, :currency, :category_id, :author, :name, :editor, :year,
+                :isbn, :image, :language,:page_nb,:description,:barcode,:print_run,:subject_code,:measure_type_code,
+                :weight, :stock_level, :height,:width ,:thickness ,:format , :cover
+
+
 end
