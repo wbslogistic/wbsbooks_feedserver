0dd1d3a8ebc04662e37d1207d86494e9cb7dfb2f
Serghei <agilestep@yahoo.com>
2014-11-11 00:34:43 +0200

    Adding ozon uploader
    * Was able to download 2.9 gb - memory and performance was ok

--- ozon_uploader.rb
+++ ozon_uploader.rb
@@ -1,2 +1,28 @@
-class OzonUploader
+
+require 'rubygems'
+require 'psych' # for reading yml
+require 'open-uri'
+
+
+@@config  =       Psych.load_file( "./config/config.yml" )
+
+
+
+
+class OzonUploader#only for style
+
+  def self.start
+    open(@@config["ozon_books_url"]) do |fin|
+      open(@@config["ozon_big_xml"] , "w") do |fout|
+        while (buf = fin.read(8192))
+          fout.write buf
+        end
+      end
+    end
+  end
+
 end
+
+
+OzonUploader.start
+
