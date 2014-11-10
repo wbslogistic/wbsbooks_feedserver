e134ee5f0e25d7ed5c5dd1b1f54f8fd02cced975
serghei <agilestep@yahoo.com>
2014-11-10 10:21:22 +0300

    just product field renamed

--- models/product.rb
+++ models/product.rb
@@ -1,6 +1,6 @@
 class Product
   attr_accessor :book_id, :price, :currency, :category_id, :author, :name, :editor, :year,
-                :isbn, :image, :language,:page_nb,:description,:barcode,:print_run,:subject_code,:measure_type_code,
+                :isbn, :image, :language,:page_count,:description,:barcode,:print_run,:subject_code,:measure_type_code,
                 :weight, :stock_level, :height,:width ,:thickness ,:format , :cover
 
 
