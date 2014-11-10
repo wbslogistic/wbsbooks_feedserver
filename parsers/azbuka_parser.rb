1e7ae374082f038e514cbf4012066346f69f725e
serghei <agilestep@yahoo.com>
2014-11-10 10:34:14 +0300

    Working on Azbuka parser, there is still a lot of garbage in the code.

--- parsers/azbuka_parser.rb
+++ parsers/azbuka_parser.rb
@@ -3,7 +3,8 @@
 class AzbukaParser
 
   def parse
-
+  
+    # temporary comented 
     # p "open ftp connection to azbuka ftp "
     # ftp = Net::FTP.new( @@config["azbuka_ftp"])
     # ftp.passive=true
@@ -14,37 +15,84 @@ class AzbukaParser
     # ftp.getbinaryfile('PraysAzbuka-Atticus.xml', @@config['azbuka_xml'] , 1024)
     # ftp.close
 
-     parse_xml (@@config["azbuka_xml"])
+    parse_with_libxml(@@config["azbuka_xml"])
+    #parse_xml (@@config["azbuka_xml"])
 
   end
 
 
   def parse_xml path
-
     products= []
-
-    #file = File.open(@xml)
     docXML = Nokogiri::XML(File.open(path))
-
     docXML.css("Product").each do |e|
       p =  Product.new
-
-      p.isbn = docXML.at.css "ProductIdentifier"
-
-
+       docXML.css("ProductIdentifier").each do |identifier|
+     end
       products << p
-
       @el = e
-      #el =XmlElement.new      #el.sku=  e.at("ProductIdentifier").content.strip.to_8
-      #el.main_image = e.at("picture").content.strip.to_8 if e.at("picture")
-      #el.category_id = e.at("categoryId").content.strip.to_8
-      #@xmlElements[el.sku] =el
     end
-
-
+    p "parsed"
   end
 
 
+
+  def parse_with_libxml path
+    reader = XML::Reader.file(path)
+    while (reader.read())
+      if (reader.name=="Product")
+         p= Product.new
+        reader.expand.children.each do |child|
+
+        if (child.name=="ProductIdentifier")
+           product_id_type = child.children.find do |child2| child2.name=="ProductIDType"  end
+
+           content_strippep =""
+           content_stripped=product_id_type.content_stripped  if product_id_type
+            if ( content_stripped=="15")
+              child.find do |child3|
+               if (child3.name=="IDValue")
+                p.barcode =  child3.content_stripped
+              end
+            end
+
+        end
+         value =  child.children.find do |subElement3| subElement3.name=="IDVALUE" end.content_stripped
+        end
+        end
+        end
+
+     p reader.to_s
+
+
+    end
+
+    end
+    end
+
+
+  #resulted in strange results -
+  def parse_with_ox path
+
+
+
+  xml1=    Ox.parse path
+
+
+  xml = Ox.parse(path)
+
+  #xml.locate('Element/foo/^Text').each do |t|
+   # @data = Model.new(:attr => t)
+    #@data.save
+  #end
+
+# or if you need to do other stuff with the element first
+  xml.locate('Product').each do |elem|
+    # do stuff
+    @data = Model.new(:attr => elem.locate('foo/^Text').first)
+    @data.save
+  end
+
+  end
 
 
 
