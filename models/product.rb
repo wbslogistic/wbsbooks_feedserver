10e8aafb1548c0059d367bfd424560620a28f812
Serghei <agilestep@yahoo.com>
2014-11-11 19:56:29 +0200

    few new properties to product

--- models/product.rb
+++ models/product.rb
@@ -1,7 +1,11 @@
 class Product
-  attr_accessor :book_id, :price, :currency, :category_id, :author, :name, :editor, :year,
+  attr_accessor :id, :book_id, :price, :currency, :category_id, :author, :name, :editor, :year,
                 :isbn, :image, :language,:page_count,:description,:barcode,:print_run,:subject_code,:measure_type_code,
-                :weight, :stock_level, :height,:width ,:thickness ,:format , :cover
+                :weight, :stock_level, :height,:width ,:thickness ,:format , :cover, :site_id
 
+  #azbuka -1
+  #exmo   -2
+  #piter  -3
+  #szko  - 4
 
 end
