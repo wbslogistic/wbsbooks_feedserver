0c377ae7dc05e760891dddeab980c278f7ff12ab
Serghei <agilestep@yahoo.com>
2014-11-14 00:07:52 +0200

    category model added

---
+++ models/category.rb
@@ -0,0 +1,28 @@
+class Category < ActiveRecord::Base
+
+
+  def self.save_categories list
+    Category.destroy_all
+    #deleting the existing ones
+
+    begin
+      #transaction
+      Category.transaction do
+        list.each do |cat|
+          cat.save(:validate => false)
+        end
+      end
+
+    rescue Exception => ex
+      Helper.log_and " Exception on saving category : " + ex.message + " trace = " + ex.backtrace.to_s
+      list.clear
+      list=nil
+      return
+    end
+    Helper.log_and " #{list.count} Categories imported from OZON !!! "
+    list.clear
+    list=nil
+  end
+end
+
+
