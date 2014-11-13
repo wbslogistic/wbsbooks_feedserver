af44eb328559e5a501d1835c6720fb756b108960
Serghei <agilestep@yahoo.com>
2014-11-14 00:06:13 +0200

    limit 2000 added
    *normally it should be in one place

--- models/o_product.rb
+++ models/o_product.rb
@@ -1,4 +1,18 @@
 class OProduct < ActiveRecord::Base
 
 
+  def product_have_more_2000
+    begin
+      return true if !self.print_run
+
+      return false if self.print_run.to_i < 2000 and self.print_run.to_s.is_number?
+    rescue Exception => ex
+      return true
+    end
+    return true
+  end
+
+
+
+
 end
