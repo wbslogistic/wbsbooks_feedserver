d52f8b5b301931bd0c3662ce611466e640c9b8c3
Serghei <agilestep@yahoo.com>
2014-11-29 19:00:11 +0200

    renaming columns

--- models/o_product.rb
+++ models/o_product.rb
@@ -3,9 +3,9 @@ class OProduct < ActiveRecord::Base
 
   def product_have_more_2000
     begin
-      return true if !self.PrintRun
+      return true if !self.printrun
 
-      return false if self.PrintRun.to_i < 2000 and self.PrintRun.to_s.is_number?
+      return false if self.printrun.to_i < 2000 and self.printrun.to_s.is_number?
     rescue Exception => ex
       return true
     end
