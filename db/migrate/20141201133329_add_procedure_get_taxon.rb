50f56dd3dfcceeed3dc1b236cd313db8d1b51895
Serghei <agilestep@yahoo.com>
2014-12-01 16:22:18 +0200

    Adding 2 procedures to generate taxons from database.

---
+++ db/migrate/20141201133329_add_procedure_get_taxon.rb
@@ -0,0 +1,33 @@
+class AddProcedureGetTaxon < ActiveRecord::Migration
+  def change
+
+
+   sql = <<-SQL
+
+        CREATE FUNCTION get_taxon(child_id integer)
+        RETURNS text
+        AS
+        $$
+         WITH RECURSIVE children AS (
+        SELECT name,self_id,parent_id, 1 AS depth                  ---|Non
+        FROM categories                    --|Recursive
+        WHERE  self_id= $1  --1137439  --name = 'Художественная литература'                    ---|Part
+        UNION ALL
+        SELECT a.name,a.self_id, a.parent_id, depth+1                   ---|Recursive
+        FROM categories a                  --|Part
+        JOIN children b ON(a.self_id = b.parent_id )    ---|
+        )
+
+        SELECT string_agg(name,'>') as taxon FROM children
+
+        $$
+        LANGUAGE SQL IMMUTABLE STRICT;
+
+
+
+    SQL
+
+    ActiveRecord::Base.connection.execute  sql
+
+  end
+end
---
+++ db/migrate/20141201140653_add_function_to_create_english_taxons.rb
@@ -0,0 +1,31 @@
+class AddFunctionToCreateEnglishTaxons < ActiveRecord::Migration
+  def change
+
+    sql = <<-SQL
+
+        CREATE FUNCTION get_taxon_en(child_id integer)
+        RETURNS text
+        AS
+        $$
+         WITH RECURSIVE children AS (
+        SELECT name_en,self_id,parent_id, 1 AS depth                  ---|Non
+        FROM categories                    --|Recursive
+        WHERE  self_id= $1  --1137439  --name = 'Художественная литература'                    ---|Part
+        UNION ALL
+        SELECT a.name_en,a.self_id, a.parent_id, depth+1                   ---|Recursive
+        FROM categories a                  --|Part
+        JOIN children b ON(a.self_id = b.parent_id )    ---|
+        )
+
+        SELECT string_agg(name_en,'>') as taxon FROM children
+
+        $$
+        LANGUAGE SQL IMMUTABLE STRICT;
+
+
+
+    SQL
+
+    ActiveRecord::Base.connection.execute  sql
+  end
+end
