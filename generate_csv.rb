78a6a1560a969ed8a44110e833b7a15dd05038b3
Serghei <agilestep@yahoo.com>
2014-12-01 16:27:52 +0200

    farther work on csv generator
    1. exchange rates are extracted and added
    2. added translation to taxons
    3. slug is created correctly
    4. isbn is fored with site number
    5. some fixes

--- generate_csv.rb
+++ generate_csv.rb
@@ -2,8 +2,39 @@ require './add_things'
 require 'csv'
 
 
+
+
+class String
+  def r_quote()
+    replace (self.gsub("&quot;"," "))
+  end
+end
+
+
+
+class CsvGenerator
+
+
+def generate
+
+ @dollar,@euro=  get_exchange_rate
+ @dollar=@dollar.to_d
+ @euro= @euro.to_d
+
+
+  create_csv_template
+
+
+  Product.where(:confirmed => 1).each_with_index do |p,i|
+    write_to_csv p,i if p.isbn and p.isbn != ""
+  end
+
+
+
+end
+
 def create_csv_template
-  columns= "name,description,slug,meta_description,meta_keywords,shipping_category_id,sku,price,Taxons,Available On,p_count,Images,product_properties,translation".split(",")
+  columns= "sku,name,description,slug,meta_description,meta_keywords,shipping_category_id,price,taxons_ru ,Taxons,Available On,p_count,Images,product_properties,translation,price_eu,price_ru".split(",")
      Helper.delete_if_exists @@config["spree_csv_path"]
     CSV.open(@@config["spree_csv_path"] , "w",{:col_sep => ","}) do |csv|
       csv << columns
@@ -11,12 +42,6 @@ def create_csv_template
 end
 
 
-
-class String
-  def r_quote()
-  replace (self.gsub("&quot;"," "))
-  end
-end
 
 
 #SKU	Name	Description	Available On	Price	CostPrice	product_properties	Taxons	OptionTypes	Variants	count_on_hand
@@ -33,26 +58,50 @@ def  write_to_csv product_obj , i
 
   product_properties = ""
   product_properties += "Publisher:" + product_obj.publisher.to_s.gsub("&quot;","''") + ";" if product_obj.publisher # this property is not necessary
-  product_properties += "Publication Year:" + product_obj.year.to_s.gsub("&quot;","''")+ ";"  if product_obj.year
-  product_properties += "ISBN:" + product_obj.isbn.to_s + ";" if product_obj.isbn
+  product_properties += "publication_year:" + product_obj.year.to_s.gsub("&quot;","''")+ ";"  if product_obj.year
+  #product_properties += "ISBN:" + product_obj.isbn.to_s + ";" if product_obj.isbn
 
-  product_properties += "Height:" + product_obj.height.to_s  + ";" if product_obj.height
-  product_properties += "Width:" + product_obj.width.to_s + ";" if product_obj.width
-  product_properties += "Weight:" + product_obj.weight.to_s + ";" if product_obj.weight
+  product_properties += "HEIGHT:" + product_obj.height.to_s  + ";" if product_obj.height
+  product_properties += "WIDTH:" + product_obj.width.to_s + ";" if product_obj.width
+  product_properties += "WEIGHT:" + product_obj.weight.to_s + ";" if product_obj.weight
 
-  product_properties += "Thickness:" + product_obj.thickness.to_s + ";" if product_obj.thickness
+  product_properties += "DEPTH:" + product_obj.thickness.to_s + ";" if product_obj.thickness
   product_properties += "Format:" + product_obj.binding.to_s  + ";" if product_obj.binding
-  product_properties += "In stock:" + product_obj.stock_level.to_s  + ";" if product_obj.stock_level
-  product_properties += "Page count:" + product_obj.pages.to_s  + ";" if product_obj.pages
+  product_properties += "in_stock:" + product_obj.stock_level.to_s  + ";" if product_obj.stock_level
+  product_properties += "Pages:" + product_obj.pages.to_s  + ";" if product_obj.pages
+
   product_properties= product_properties.gsub(";","|")
 
 
 #-------------------TAXONS------------------------------------------
 
-  taxons ="Categories>Business books|"
-  taxons += "Authors>" + product_obj.author.r_quote +  "|" if product_obj.author
-  taxons += "Publishers>" + product_obj.publisher.r_quote if product_obj.publisher
-  taxons.strip
+  #taxons ="Categories>Business books|"
+
+  taxon_en = ""
+  taxon_en += product_obj.taxon_en.split("|").map do |tax|
+    "Categories>" + tax + "|"
+  end.join if  product_obj.taxon_en
+
+
+   taxon_en = "Undefined|" if taxon_en == ""
+
+
+  taxon_en += "Authors>" + product_obj.author.r_quote +  "|" if product_obj.author
+  taxon_en += "Publishers>" + product_obj.publisher.r_quote if product_obj.publisher
+  taxon_en.strip
+
+
+  taxon_ru = ""
+  taxon_ru=  product_obj.taxon_ru.split("|").map do |tax|
+    "Категорий>" + tax + "|"
+  end.join if  product_obj.taxon_ru
+
+  taxon_ru = "Undefined|" if taxon_ru == ""
+
+  taxon_ru += "Авторы>" + product_obj.author.r_quote +  "|" if product_obj.author
+  taxon_ru += "Издатели>" + product_obj.publisher.r_quote if product_obj.publisher
+  taxon_ru.strip
+
 
 #-------------------TRANSLATION------------------------------------------
 
@@ -64,89 +113,80 @@ def  write_to_csv product_obj , i
   translation_text = translation_title + ":"  + translation_description
   translation_text = nil if translation_text==":"
 
-  values =  [product_obj.titleru.gsub("\n"," </br> "), product_obj.descriptionru.gsub("\n"," </br> "),"slug_"+
-             (182+i).to_s,"meta description","meta keyword","default",product_obj.isbn, product_obj.price,taxons,"2014-01-01",33,image_url,product_properties,translation_text]
+
+  site_id = "_site_" + product_obj.site_id.to_s[-1]
+  slug = "book_"  + product_obj.id.to_s + site_id #this is for case when no translation done
+
+
+
+  slug=translation_title.gsub(" ","_") + site_id  if translation_title != ""
+
+  isbn = product_obj.isbn +  site_id
+
+
+  price_rub =   product_obj.price
+  price_us =   (product_obj.price / @dollar).to_d.round(2)
+  price_euro =   (product_obj.price / @euro).to_d.round(2)
+
+
+
+  values =  [ isbn ,product_obj.titleru.gsub("\n"," </br> "), product_obj.descriptionru.gsub("\n"," </br> "),slug,
+             translation_description,translation_title,"default", price_us,taxon_ru,taxon_en,"2014-01-01",product_obj.stock_level,image_url,product_properties,translation_text,price_euro,price_rub]
 
              #stock_level,p.price,p.price,"","general","","", "default"]
 
-  #values = values +  [p.variants.to_s,p.v_SKU.to_s,  p.v_price.to_s,p.v_length.to_s,p.v_weight.to_s,p.v_height.to_s,p.v_depth.to_s,p.v_width.to_s,p.v_diameter.to_s,p.v_pack_height.to_s,p.v_pack_width.to_s,p.pack_height.to_s,p.pack_weight.to_s,p.v_images.to_s.to_s ] if  p.product_variants && p.product_variants.count > 0
+
   CSV.open(@@config["spree_csv_path"], "a+",{:col_sep => ","}) do |csv|
     csv <<  values
   end
 end
 
-ActiveRecord::Base.logger = Logger.new(STDOUT)
-
-
-create_csv_template
-
-
-products =  Product.where(:confirmed => 1)
 
 
 
-products.each_with_index do |p,i|
 
 
-   write_to_csv p,i
+def get_exchange_rate
+
+
+  dollar = nil
+  euro =nil
+
+
+  n_times "Problem with getting exchange rate " do
+
+  now= DateTime.now
+
+      bank_url ="http://www.cbr.ru/scripts/XML_daily.asp?date_req=#{format("%.2i", now.day)}/#{format("%.2i",now.month)}/#{now.year}"
+      result =  HTTParty.get(bank_url)
+
+          result["ValCurs"]['Valute'].each do |value|
+
+                   if  value['CharCode']=='USD'
+                     dollar =    value['Value']
+                   end
+                   if  value['CharCode']=='EUR'
+                     euro =    value['Value']
+                   end
+          end
+
+end
+  return dollar.gsub(",","."),euro.gsub(",",".")
+
+
+ end
+
+
+
+
 end
 
 
 
+ActiveRecord::Base.logger = Logger.new(STDOUT)
 
 
 
 
-
-
-
-#working sql !!!
-# Copy (SELECT isbn as "SKU" ,
-#                      "titleRU" as "Name",
-#                                   "descriptionRU" as Description,
-#                                                      'True' as "Available On",
-#                                                                price as "Price",
-#                                                                         'NoATaxonYet' as Taxons ,
-#                                                                                          currency,
-#                                                                                          "Author",
-#                                                                                          "Publisher",
-#                                                                                          "Year",
-#                                                                                          Weight
-# FROM products as t1
-#
-# limit 10)
-# To '/tmp/result.csv' With  csv HEADER;
-#
-
-
-
-
-# sql = %q[SELECT isbn as "SKU" ,
-#      'titleRU' as "Name",
-#      'descriptionRU' as Description,
-#      'True' as "Available On",
-#      price as "Price",
-#      'NoATaxonYet' as Taxons ,
-#      currency,
-#       "Author",
-#      "Publisher",
-#      "Year",
-#      Weight
-#   FROM products as t1
-#
-#   limit 10;
-#
-# "] # Single quoting
-#
-
-#
-# columns= ["SKU", "Recalculate", "Name", "Designer", "Type_of_product", "Description", "Available On",
-#  "shipping_category", "Price", "CostPrice", "product_properties",
-#  "Length", "Weight", "Height", "Depth", "Width", "Diameter",
-#  "Pack_length", "Pack_width", "Pack_height", "Pack_weight",
-#  "Taxons", "Images", "Variants", "Variant_sku", "Variant_price",
-#  "Variant_length", "Variant_weight", "Variant_height", "Variant_depth",
-#  "Variant_width", "Variant_diameter", "Variant_pack_length", "Variant_pack_width",
-#  "variant_pack_height", "Variant_pack_weight", "Variant_images" ]
-
+CsvGenerator.new.generate
 
